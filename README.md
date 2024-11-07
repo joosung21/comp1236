96eeaf3cbd60839eed81d90d72caede3d46f1de2
Joosung Ahn <joosung21@gmail.com>
2024-11-07 12:52:02 -0500

    Init Project

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# comp1236
+# comp1236 Group Project
+
+## Group
+MM_Group 27
+- Sepasian, Kiana
+- Ahn, Joosung
+- Choi, Junyong
+
+## Due date
+December 6 Friday, 2024
+
+## Group Project Steps
+1. Practice your own programming with *yourname/workspace.sh* which is already exist in the repo.
+2. Commit and sync/push to share for group memebers.
+3. We will review and discuss the codes offline.
+4. Combine our codes to *Group_27_Application.sh*
+5. We will discuss offline about flowchard and personal document later.
+
+(Let's talk the details offline on next Tuesday at School)
+
+## Submit
+*Group_No_Project.zip* contains three files:
+
+1. Group_27_Flowchart.pdf
+2. Group_27_Application.sh
+3. Document where you have indicated the software used for completion of your tasks and how the work was distributed amongst team members.
+
+## D2L Page
+[Group Project Assignment (Majlinda's in-person groups)](https://learn.georgebrown.ca/d2l/le/content/292339/viewContent/9359798/View)
+
+## Instruction document
+[Group Project_Fall'24.pdf](/docs/Group%20Project_Fall'24.pdf)
---
+++ joosung/workspace.sh
@@ -0,0 +1,192 @@
+# Check Password
+password="App1"
+attempts=3
+
+while [ $attempts -gt 0 ]; do
+    echo -n "Enter password: "
+    read password_input
+
+    if [ "$password_input" == "$password" ]; then
+        break
+    else
+        attempts=$((attempts - 1))
+        if [ $attempts -eq 0 ]; then
+            echo "Access denied."
+            exit 1
+        else
+            echo "Incorrect password. Please try again."
+        fi
+    fi
+done
+
+echo
+echo "*******************************"
+echo "      Group 27 Application     "
+echo "*******************************"
+
+# Display the menu
+displayMenu() {
+    echo
+    echo "Please select a task to perform:"
+    echo "A) Triangular Numbers"
+    echo "B) Successive Products"
+    echo "C) Sequence Calculation"
+    echo -n "Enter your choice: "
+}
+
+# Ask to continue
+continuePrompt() {
+    echo
+    echo -n "Would you like to perform another task? (y/n): "
+    read continue_choice
+    if [[ "$continue_choice" != "y" && "$continue_choice" != "Y" ]]; then
+        echo
+        echo "Bye!"
+        echo
+        exit 0
+    fi
+}
+
+# Task 1: Triangular Numbers
+task1() {
+    echo
+    echo "*******************************"
+    echo "       Triangular Numbers      "
+    echo "*******************************"
+    echo "Please enter the range of numbers to check for triangular numbers."
+    echo -n "Range start: "
+    read start
+    echo -n "Range end: "
+    read end
+
+    odd_product=1
+    even_product=1
+    odd_count=0
+    even_count=0
+    echo
+    echo "Triangular numbers:"
+
+    n=1
+    while true; do
+        tri_num=$((n * (n + 1) / 2))
+        if [ $tri_num -gt $end ]; then
+            break
+        elif [ $tri_num -ge $start ]; then
+            if ((tri_num % 2 == 0)); then
+                echo "$tri_num"
+                even_count=$((even_count + 1))
+                even_product=$((even_product * tri_num))
+            else
+                echo "$tri_num"
+                odd_count=$((odd_count + 1))
+                odd_product=$((odd_product * tri_num))
+            fi
+        fi
+        n=$((n + 1))
+    done
+
+    echo
+    echo "Product of odd: $odd_product"
+    echo "Product of even: $even_product"
+
+    continuePrompt
+}
+
+# Task 2: Successive Products
+task2() {
+    echo
+    echo "*******************************"
+    echo "       Successive Products     "
+    echo "*******************************"
+    echo "Please enter the starting number, the number of terms, and the number for factor checking."
+    echo -n "Starting number: "
+    read start
+    echo -n "how many numbers want to print: "
+    read count
+    echo -n "Number for factor checking: "
+    read x
+
+    echo
+    echo "Product of two natural numbers in succession (factor check):"
+
+    for ((i=0; i<count; i++)); do
+        product=$((start * (start + 1)))
+        if ((x % product == 0)); then
+            echo $product "(True)"
+        else
+            echo $product "(False)"
+        fi
+        start=$((start + 1))
+    done
+
+    continuePrompt
+}
+
+# Task 3: Sequence Calculation
+task3() {
+    echo
+    echo "*******************************"
+    echo "       Sequence Calculation      "
+    echo "*******************************"
+    echo "Term = an^3 + bn + c"
+    echo "Please enter the values for a, b, c, and the range [n1, n2]."
+    echo -n "a: "
+    read a
+    echo -n "b: "
+    read b
+    echo -n "c: "
+    read c
+    echo -n "Start of range (n1): "
+    read n1
+    echo -n "End of range (n2): "
+    read n2
+
+    first_term=0
+    last_term=0
+
+    echo
+    echo "Terms of the sequence:"
+    for ((n=n1; n<=n2; n++)); do
+        term=$((a * n**3 + b * n + c))
+        echo $term
+        if ((n == n1)); then
+            first_term=$term
+        fi
+        last_term=$term
+    done
+
+    echo
+
+    product=$((first_term * last_term))
+    echo "Product of first and last term: $product"
+    if ((product % 4 == 0)); then
+        echo "The product is a multiple of 4."
+    else
+        echo "The product is not a multiple of 4."
+    fi
+
+    continuePrompt
+}
+
+
+while true; do
+    displayMenu
+    read choice
+
+    case $choice in
+        A|a)
+            task1
+            ;;
+        B|b)
+            task2
+            ;;
+        C|c)
+            task3
+            ;;
+        *)
+            echo "Invalid choice! Please try again."
+            ;;
+    esac
+done
+
+
